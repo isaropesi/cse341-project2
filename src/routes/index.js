0fe6a4da07187e41ab10351a24770e656d35cd1e
isaropesi <sil22028@byui.edu>
2026-01-25 00:40:36 -0300

    Redirect root to api-docs for a better landing page experience

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/', require('./swagger'));
+router.get('/', (req, res) => {
+    res.redirect('/api-docs');
+});
 router.use('/books', require('./books'));
 router.use('/authors', require('./authors'));
 
